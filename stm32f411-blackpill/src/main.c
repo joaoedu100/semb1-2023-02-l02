9807a31410095ff8bcaebfc00d28c9d406196601
joaoedu100 <107135915+joaoedu100@users.noreply.github.com>
2024-04-21 14:31:53 -0300

    Update main.c

--- stm32f411-blackpill/src/main.c
+++ stm32f411-blackpill/src/main.c
@@ -25,31 +25,39 @@
 
 #define STM32_GPIOC_BASE     0x40020800     /* 0x48000800-0x48000bff: GPIO Port C */
 
+/* GPIOA Base Addresses ******************************************************/
+
+#define STM32_GPIOA_BASE     0x40020000     /* 0x48000000-0x480003ff: GPIO Port A*/
+
 /* Register Offsets *********************************************************/
 
 #define STM32_RCC_AHB1ENR_OFFSET  0x0030   /* AHB1 Peripheral Clock enable register */
-
 #define STM32_GPIO_MODER_OFFSET   0x0000  /* GPIO port mode register */
 #define STM32_GPIO_OTYPER_OFFSET  0x0004  /* GPIO port output type register */
 #define STM32_GPIO_PUPDR_OFFSET   0x000c  /* GPIO port pull-up/pull-down register */
 #define STM32_GPIO_ODR_OFFSET     0x0014  /* GPIO port output data register */
 #define STM32_GPIO_BSRR_OFFSET    0x0018  /* GPIO port bit set/reset register */
-
+#define STM32_GPIO_IDR_OFFSET     0x0010
 
 /* Register Addresses *******************************************************/
 
 #define STM32_RCC_AHB1ENR        (STM32_RCC_BASE+STM32_RCC_AHB1ENR_OFFSET)
-
 #define STM32_GPIOC_MODER        (STM32_GPIOC_BASE+STM32_GPIO_MODER_OFFSET)
 #define STM32_GPIOC_OTYPER       (STM32_GPIOC_BASE+STM32_GPIO_OTYPER_OFFSET)
 #define STM32_GPIOC_PUPDR        (STM32_GPIOC_BASE+STM32_GPIO_PUPDR_OFFSET)
 #define STM32_GPIOC_ODR          (STM32_GPIOC_BASE+STM32_GPIO_ODR_OFFSET)
 #define STM32_GPIOC_BSRR         (STM32_GPIOC_BASE+STM32_GPIO_BSRR_OFFSET)
+#define STM32_GPIOA_MODER        (STM32_GPIOA_BASE+STM32_GPIO_MODER_OFFSET)
+#define STM32_GPIOA_OTYPER       (STM32_GPIOA_BASE+STM32_GPIO_OTYPER_OFFSET)
+#define STM32_GPIOA_PUPDR        (STM32_GPIOA_BASE+STM32_GPIO_PUPDR_OFFSET)
+#define STM32_GPIOA_IDR          (STM32_GPIOA_BASE+STM32_GPIO_ODR_OFFSET)
+#define STM32_GPIOA_BSRR         (STM32_GPIOA_BASE+STM32_GPIO_BSRR_OFFSET)
+
 
 /* AHB1 Peripheral Clock enable register */
 
 #define RCC_AHB1ENR_GPIOCEN      (1 << 2)  /* Bit 2:  IO port C clock enable */
-
+#define RCC_AHB1ENR_GPIOAEN      (1 << 0)
 /* GPIO port mode register */
 
 #define GPIO_MODER_INPUT           (0) /* Input */
@@ -119,8 +127,9 @@ int main(int argc, char *argv[])
   uint32_t *pGPIOC_PUPDR  = (uint32_t *)STM32_GPIOC_PUPDR;
   uint32_t *pGPIOC_BSRR   = (uint32_t *)STM32_GPIOC_BSRR;
   uint32_t *pGPIOA_MODER = (unint32_t *)STM32_GPIOA_MODER;
-  uint32_t *PGPIOA_IDR = (uint32_t *)STM32_GPIOA_IDR;
- 
+  uint32_t *pGPIOA_OTYPER = (uint32_t *)STM32_GPIOA_OTYPER;
+  uint32_t *pGPIOA_PUPDR  = (uint32_t *)STM32_GPIOA_PUPDR;
+  uint32_t *pGPIOA_BSRR   = (uint32_t *)STM32_GPIOA_BSRR;
   /* Habilita clock GPIOC */
 
   reg  = *pRCC_AHB1ENR;
